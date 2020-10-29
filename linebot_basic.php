330535bfc213b141d4ff7d4ca195abe380c89fcb
61160061 <70052005+61160061@users.noreply.github.com>
2020-10-29 16:11:05 +0700

    Update linebot_basic.php

--- linebot_basic.php
+++ linebot_basic.php
@@ -14,10 +14,11 @@ foreach ($request_json['events'] as $event)
 		{
 			$text = $event['message']['text'];
 			
+			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';
 			$reply_message = 'ฉันได้รับข้อความ '. $text.' ของคุณแล้ว!';   
 			
 		} else {
-			$reply_message = 'ฉันได้รับ '.$event['message']['type'].' ของคุณแล้ว!';
+			$reply_message = 'gus-gus';
 		}
 	} else {
 		$reply_message = 'ฉันได้รับ Event '.$event['type'].' ของคุณแล้ว!';
